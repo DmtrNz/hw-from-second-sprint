3e00c4ca586d9ac53a70f4f58b8757be405f2a38
DmtrNz <naz.dmtr@gmail.com>
2025-01-18 15:39:46 +0300

    before refactoring types and adding styles

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -1,37 +1,55 @@
 import React, { ChangeEvent, KeyboardEvent, useState } from 'react'
 import Greeting from './Greeting'
 import { UserType } from './HW3'
+/*
+* 4 - в файле GreetingContainer.tsx дописать типизацию пропсов DONE
+* 5 - в файле GreetingContainer.tsx указать нужные типы в useState с name и error DONE
+* 6 - в файле GreetingContainer.tsx дописать тип и логику функции setNameCallback
+* 7 - в файле GreetingContainer.tsx дописать логику функций pureAddUser, pureOnBlur, pureOnEnter и проверить их тестами
+* 8 - в файле GreetingContainer.tsx вычислить количество добавленных и имя последнего (totalUsers, lastUserName) DONE
+*/
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: UserType[] // need to fix any DONE
+    addUserCallback: (name: string) => void // need to fix any DONE
 }
 
-export const pureAddUser = (name: any, setError: any, setName: any, addUserCallback: any) => {
-    // если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
-}
+export const pureAddUser = (name: string, setError: any, setName: any, addUserCallback: any) => {
+    if (name.trim()) {
+        addUserCallback(name);
+        setError("");
+        setName("");
+    } else {
+        setError("Ошибка! Введите имя!");
+        if (name !== "") {
+            setName(name);
+        }
+    }
+}// если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут DONE
 
-export const pureOnBlur = (name: any, setError: any) => { // если имя пустое - показать ошибку
-}
+export const pureOnBlur = (name: string, setError: any) => { 
+    if(!name.trim()){
+        setError("Ошибка! Введите имя!")
+    }
+}// если имя пустое - показать ошибку DONE
 
-export const pureOnEnter = (e: any, addUser: any) => { // если нажата кнопка Enter - добавить
-}
+export const pureOnEnter = (e: KeyboardEvent<HTMLInputElement>, addUser: any) => { 
+    if(e.key==="Enter" ){
+        addUser()
+    }
+}// если нажата кнопка Enter - добавить DONE
 
-// более простой и понятный для новичков
-// function GreetingContainer(props: GreetingPropsType) {
 
-// более современный и удобный для про :)
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
     users,
     addUserCallback,
 }) => {
     // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+    const [name, setName] = useState<string>('') // need to fix any DONE
+    const [error, setError] = useState<string>('') // need to fix any DONE
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('some name') // need to fix
-
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any DONE
+        setName(e.currentTarget.value) // need to fix DONE
         error && setError('')
     }
     const addUser = () => {
@@ -42,12 +60,12 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
         pureOnBlur(name, setError)
     }
 
-    const onEnter = (e: any) => {
+    const onEnter = (e: KeyboardEvent<HTMLInputElement>) => {
         pureOnEnter(e, addUser)
     }
 
-    const totalUsers = 0 // need to fix
-    const lastUserName = 'some name' // need to fix
+    const totalUsers = users.length // need to fix DONE
+    const lastUserName = users.length > 0 ? users[users.length - 1].name : '' // need to fix DONE
 
     return (
         <Greeting
--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -4,9 +4,10 @@ import s2 from '../../s1-main/App.module.css'
 import GreetingContainer from './GreetingContainer'
 
 /*
-* 1 - описать тип UserType
-* 2 - указать нужный тип в useState с users
-* 3 - дописать типы и логику функции pureAddUserCallback и проверить её тестами
+* 1 - описать тип UserType DONE
+* 2 - указать нужный тип в useState с users DONE
+* 3 - дописать типы и логику функции pureAddUserCallback и проверить её тестами DONE
+
 * 4 - в файле GreetingContainer.tsx дописать типизацию пропсов
 * 5 - в файле GreetingContainer.tsx указать нужные типы в useState с name и error
 * 6 - в файле GreetingContainer.tsx дописать тип и логику функции setNameCallback
@@ -19,20 +20,22 @@ import GreetingContainer from './GreetingContainer'
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: string // need to fix any DONE
+    name: string // need to fix any DONE
 }
 
-export const pureAddUserCallback = (name: any, setUsers: any, users: any) => { // need to fix any
-    const user = { // need to fix
-    }
+export const pureAddUserCallback = (name: string, setUsers: any, users: UserType[]) => { // !!! need to fix any to React.Dispatch<React.SetStateAction<UserType[]>>
+    const user = { 
+        _id: v1(),
+        name: name
+    } // need to fix DONE
     setUsers([...users, user])
 }
 
 const HW3 = () => {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+    const [users, setUsers] = useState<Array<UserType>>([]) // need to fix any DONE
 
-    const addUserCallback = (name: any) => { // need to fix any
+    const addUserCallback = (name: string) => { // need to fix any DONE
         pureAddUserCallback(name, setUsers, users)
     }
 
