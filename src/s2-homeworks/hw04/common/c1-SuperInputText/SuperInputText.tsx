7e7ad7db761182e89f89c3fb582b5000a44e8cab
DmtrNz <naz.dmtr@gmail.com>
2025-01-24 20:26:16 +0300

    HW4

--- src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
+++ src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
@@ -13,6 +13,7 @@ type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElem
 
 // здесь мы говорим что у нашего инпута будут такие же пропсы как у обычного инпута, кроме type
 // (чтоб не писать value: string, onChange: ...; они уже все описаны в DefaultInputPropsType)
+
 type SuperInputTextPropsType = Omit<DefaultInputPropsType, 'type'> & {
     // и + ещё пропсы которых нет в стандартном инпуте
     onChangeText?: (value: string) => void
@@ -25,31 +26,34 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = (
     {
         onChange,
         onChangeText,
-        onKeyDownCapture,
+        onKeyPress,
         onEnter,
         error,
         className,
         spanClassName,
         id,
-
         ...restProps // все остальные пропсы попадут в объект restProps
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        onChange?.(e) // если есть пропс onChange, то передать ему е (поскольку onChange не обязателен)
-
+         // если есть пропс onChange, то передать ему е (поскольку onChange не обязателен)
+        onChange?.(e);
         onChangeText?.(e.currentTarget.value)
     }
-    const onKeyDownCaptureCallback = (e: KeyboardEvent<HTMLInputElement>) => {
-        onKeyDownCapture?.(e)
+    const onKeyPressCallback = (e: KeyboardEvent<HTMLInputElement>) => {
+        onKeyPress?.(e)
 
         onEnter && // если есть пропс onEnter
         e.key === 'Enter' && // и если нажата кнопка Enter
         onEnter() // то вызвать его
     }
 
+
+
+
     const finalSpanClassName = s.error
-        + (spanClassName ? ' ' + spanClassName : '')
+        + (spanClassName ? ' ' + spanClassName : '');
+
     const finalInputClassName = s.input
         + (error ? ' ' + s.errorInput : ' ' + s.superInput)
         + (className ? ' ' + className : '') // задача на смешивание классов
@@ -60,7 +64,7 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = (
                 id={id}
                 type={'text'}
                 onChange={onChangeCallback}
-                onKeyDownCapture={onKeyDownCaptureCallback}
+                onKeyPress={onKeyPressCallback}
                 className={finalInputClassName}
                 {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
             />
